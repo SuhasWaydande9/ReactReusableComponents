aff89188f5623574f24f9836fd538a02b414edf5
RudrahTheDestroyer <40670485+SuhasWaydande9@users.noreply.github.com>
2020-11-06 09:05:13 +0530

    Update Navbar Section

--- src/components/Navbars/NavbarSection.js
+++ src/components/Navbars/NavbarSection.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar1 from './navbar1/navbar1.js';
 import Navbar2 from './navbar2/navbar2.js';
 import Navbar3 from './navbar3/navbar3.js';
+import Navbar4 from './Navbar/Navbar.js';
 import "./MainStyle.scss"
 
 class NavbarSection extends React.Component {
@@ -12,6 +13,7 @@ class NavbarSection extends React.Component {
                 <Navbar1 />
                 <Navbar2 />
                 <Navbar3 />
+                <Navbar4 />
             </div>
         )
     }
