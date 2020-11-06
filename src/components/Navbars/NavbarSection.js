cd5ea62803d57382af482d3f34745265556ca607
RudrahTheDestroyer <40670485+SuhasWaydande9@users.noreply.github.com>
2020-11-06 09:49:13 +0530

    Deleted Some Changes

--- src/components/Navbars/NavbarSection.js
+++ src/components/Navbars/NavbarSection.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Navbar1 from './navbar1/navbar1.js';
 import Navbar2 from './navbar2/navbar2.js';
 import Navbar3 from './navbar3/navbar3.js';
-import Navbar4 from './Navbar/Navbar.js';
 import "./MainStyle.scss"
 
 class NavbarSection extends React.Component {
@@ -13,9 +12,6 @@ class NavbarSection extends React.Component {
                 <Navbar1 />
                 <Navbar2 />
                 <Navbar3 />
-                <div className="NavbarContainer">
-                    <Navbar4 />
-                </div>
             </div>
         )
     }
